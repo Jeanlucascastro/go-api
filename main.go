1606fbdec84fd8f8b0d344b04ad58542a2d36d4e
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-04-14 18:10:26 -0300

    Associations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"go-api/model"
 	"log"
 	"net/http"
@@ -28,18 +29,77 @@ func salvarPedidoHander(w http.ResponseWriter, r *http.Request) {
 	}
 
 	db.AutoMigrate(&model.Order{})
+	db.AutoMigrate((&model.Item{}))
 
+
+	tx := db.Begin()
+	defer func() {
+			if r := recover(); r != nil {
+					tx.Rollback()
+					http.Error(w, "Error saving order: "+ fmt.Sprint(r), http.StatusInternalServerError)
+					return
+			}
+	}()
+
+	// Efficiently upsert items (avoid redundant lookups)
+	itemMap := make(map[string]model.Item)
+	for _, item := range order.Item {
+			if existingItem, ok := itemMap[item.Name]; ok {
+					order.Item = append(order.Item, existingItem) // Use existing item
+			} else {
+					var newItem model.Item
+					if err := tx.FirstOrCreate(&newItem, model.Item{Name: item.Name}).Error; err != nil {
+							tx.Rollback()
+							http.Error(w, "Error upserting item: "+err.Error(), http.StatusInternalServerError)
+							return
+					}
+					itemMap[item.Name] = newItem
+					order.Item = append(order.Item, newItem)
+			}
+	}
 
 	if err := db.Create(&order).Error; err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+
+
+	// w.WriteHeader(http.StatusOK)
+
+	response := struct {
+		Message string `json:"message"`
+		OrderID uint   `json:"order_id"`
+	} {
+		Message: "Pedido Salvo",
+		OrderID: order.ID,
+	}
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func buscarPedidosHandler(w http.ResponseWriter, r *http.Request) {
+
+	dsn := "host=localhost user=postgres password=postgres dbname=talosorder port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var orders []model.Order
+	if err := db.Preload("Item").Find(&orders).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(orders)
 
 }
 
 func main() {
 	http.HandleFunc("/order", salvarPedidoHander)
+	http.HandleFunc("/orders", buscarPedidosHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- model/order.go
+++ model/order.go
@@ -1,8 +1,8 @@
 package model
 
 type Order struct {
-	ID uint `gorm:"primaryKey;autoIncrement"`
-	// Item   []Item
+	ID     uint   `gorm:"primaryKey;autoIncrement"`
+	Item   []Item `gorm:"many2many:order_items;"`
 	Total  float64
 	Status string
 }
