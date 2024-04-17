ae5c9e9bf723eb416c4d2c3e7d8d2e143e45e09b
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-04-16 21:04:38 -0300

    Using Gin Framework

--- main.go
+++ main.go
@@ -1,105 +1,49 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"go-api/model"
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func salvarPedidoHander(w http.ResponseWriter, r *http.Request) {
-
-	var order model.Order
-	err := json.NewDecoder(r.Body).Decode(&order)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-
-	dsn := "host=localhost user=postgres password=postgres dbname=talosorder port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	db.AutoMigrate(&model.Order{})
-	db.AutoMigrate((&model.Item{}))
-
-
-	tx := db.Begin()
-	defer func() {
-			if r := recover(); r != nil {
-					tx.Rollback()
-					http.Error(w, "Error saving order: "+ fmt.Sprint(r), http.StatusInternalServerError)
-					return
-			}
-	}()
-
-	// Efficiently upsert items (avoid redundant lookups)
-	itemMap := make(map[string]model.Item)
-	for _, item := range order.Item {
-			if existingItem, ok := itemMap[item.Name]; ok {
-					order.Item = append(order.Item, existingItem) // Use existing item
-			} else {
-					var newItem model.Item
-					if err := tx.FirstOrCreate(&newItem, model.Item{Name: item.Name}).Error; err != nil {
-							tx.Rollback()
-							http.Error(w, "Error upserting item: "+err.Error(), http.StatusInternalServerError)
-							return
-					}
-					itemMap[item.Name] = newItem
-					order.Item = append(order.Item, newItem)
-			}
-	}
-
-	if err := db.Create(&order).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-
-
-	// w.WriteHeader(http.StatusOK)
-
-	response := struct {
-		Message string `json:"message"`
-		OrderID uint   `json:"order_id"`
-	} {
-		Message: "Pedido Salvo",
-		OrderID: order.ID,
-	}
-	json.NewEncoder(w).Encode(response)
-
-}
-
-func buscarPedidosHandler(w http.ResponseWriter, r *http.Request) {
-
-	dsn := "host=localhost user=postgres password=postgres dbname=talosorder port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var orders []model.Order
-	if err := db.Preload("Item").Find(&orders).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(orders)
-
-}
-
 func main() {
-	http.HandleFunc("/order", salvarPedidoHander)
-	http.HandleFunc("/orders", buscarPedidosHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	dsn := "host=localhost user=postgres password=postgres dbname=talosorder port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+
+	db.AutoMigrate(&model.Order{}, &model.Item{})
+
+	r := gin.Default()
+
+	r.POST("/order", func(c *gin.Context) {
+		var order model.Order
+		if err := c.BindJSON(&order); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := db.Create(&order).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Pedido Salvo", "order_id": order.ID})
+	})
+
+	r.GET("/orders", func(c *gin.Context) {
+		var orders []model.Order
+		if err := db.Preload("Item").Find(&orders).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, orders)
+	})
+
+	log.Fatal(r.Run(":8080"))
 }
